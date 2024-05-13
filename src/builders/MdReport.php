9b6f0ed5f283dbb23ad11746afbbee569d709f26
drweb_000 <drweb_000@user-pc>
2024-05-13 13:19:35 +0300

    Исключены отложенные задачи

--- src/builders/MdReport.php
+++ src/builders/MdReport.php
@@ -14,6 +14,11 @@ class MdReport extends AbstractBuilder {
         $sth = DB::prepare($this->getSqlQuery());
         $sth->execute([$params['group'], $params['group']]);
         
+        $sth->nextRowset();
+        $sth->nextRowset();
+        $sth->nextRowset();
+        $sth->nextRowset();
+
         $result = [];
         while ($row = $sth->fetch()) {
             $result[] = [
@@ -33,6 +38,10 @@ class MdReport extends AbstractBuilder {
     
     protected function getSqlQuery() {
         return <<<FIN
+            DROP TEMPORARY TABLE IF EXISTS vt_topics;
+            DROP TEMPORARY TABLE IF EXISTS vt_t2;
+
+            CREATE TEMPORARY TABLE vt_topics
             SELECT 
                 IFNULL(chat.title, 'НЕОПРЕДЕЛЕНО') AS chat_title,
                 topic.chat_id AS chat_id,
@@ -40,7 +49,9 @@ class MdReport extends AbstractBuilder {
                 topic.is_task AS is_task,
                 topic.is_urgent AS is_urgent,
                 topic.topic_title AS topic_title,
-                topic.topic_id AS topic_id
+                topic.topic_id AS topic_id,
+                topic.wait_for AS wait_for,
+                topic.wait_till AS wait_till
             FROM 
                 [topics] AS topic 
                 LEFT JOIN [telle_chats] AS chat ON chat.id = topic.chat_id 
@@ -48,9 +59,20 @@ class MdReport extends AbstractBuilder {
                 topic.status < 100 
                 AND topic.chat_id IN (
                         SELECT chat_group.chat_id FROM [chat_groups] AS chat_group WHERE ? = 'all' OR chat_group.chat_group = ?
-                    )
+                    );
+        
+            CREATE TEMPORARY TABLE vt_t2 SELECT topic_id FROM vt_topics;
+        
+            SELECT 
+                *
+            FROM 
+                vt_topics
+            WHERE 
+                wait_for NOT IN (SELECT topic_id FROM vt_t2)
+                AND (wait_till IS NULL OR wait_till < NOW())
             ORDER BY
-                is_task, status, is_urgent DESC, chat_title
+                is_task, status, is_urgent DESC, chat_title;
+        
             FIN;
     }
 }
